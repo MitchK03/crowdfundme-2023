6e1bab8365e18a252be224ef465d60ecdc37977b
MitchK03 <mitchellkastein@gmail.com>
2023-09-05 20:01:54 -0500

    made project routes, templates

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { Project } = require('../models')
+const { Project, User } = require('../models')
 
 // homepage
 router.get('/', async (req, res) => {
@@ -11,6 +11,18 @@ router.get('/', async (req, res) => {
   } catch(err) {
     res.status(500).json(err)
   }
+})
+
+router.get('/projects/:id', async(req, res) =>{
+try {
+  const project = await Project.findByPk(req.params.id, {
+    include: [{model: User}],
+    raw: true,
+  });
+  res.render('project', {...project, fundingReached: project.needed_funding === 0})
+}catch (err) {
+  res.status(500).json(err);
+}
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
-const { engine } = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,7 +9,14 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', engine())
+const hbs = exphbs.create({
+  helpers: {
+    format_date: function(date) {
+      return new Date(date).toLocaleDateString()
+    }
+  }
+})
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 app.set('views', './views')
 
